045ce646525147071e1186c07c7b9f4f972c99b5
eit <cheneit@gmail.com>
2014-10-21 19:11:06 +0800

    update balance() in BST

--- src/BinarySearchTree.java
+++ src/BinarySearchTree.java
@@ -58,62 +58,69 @@ public class BinarySearchTree {
 
 	// balance this tree
 	public void balance() {
-		if (NodeCount <= 1)
+		if (NodeCount <= 1) {
 			return;
-
-		// STRIP ITEMS FROM TREE AS LIST
-
-		else {
-			// Taking advantage of Java's generic collections
-			Deque<Node> stack = new ArrayDeque<Node>();
-			Node cur = root, front = null, tail = null;
-
-			// In-order traversal via a stack (after Carrano)
-
-			while (true) // Infinite loop exited via break Carrano code is
-							// equivalent in behavior to a "while"
-			{
-				while (cur != null) {
-					stack.push(cur);
-					cur = cur.left;
-				}
-				// What follows would be under Carrano's "else"
-				if (stack.isEmpty())
-					break; // Exit the processing loop
-				else {
-					cur = stack.pop();
-					if (front == null)
-						front = tail = cur;
-					else
-						tail = tail.right = cur;
-					cur.left = null;
-					cur = cur.right;
-				}
-			}
-			root = front;
-
-			// FORM BALANCED TREE --- after Stout and Warren
-
-			// Pseudo-root used; real root to the right
-			Node pseudo = new Node(-1);
-			int m, nBack = NodeCount - 1;
-
-			pseudo.right = root;
-			for (m = nBack / 2; m > 0; m = nBack / 2) {
-				int j = 0;
-				Node scanner = pseudo;
-
-				for (j = 0; j < m; j++) {// leftward rotation
-					Node child = scanner.right;
-					scanner.right = child.right;
-					scanner = scanner.right;
-					child.right = scanner.left;
-					scanner.left = child;
-				} // end for j . . .
-				nBack = nBack - m - 1;
-			} // end for m . . .
-			root = pseudo.right;
+		} else {
+			// Start to balance
+			a();
+			b();
 		}
 		return;
 	}
+
+	private void a() {
+		Deque<Node> stack = new ArrayDeque<Node>();
+		Node cur = root, front = null, tail = null;
+
+		// In-order traversal via a stack (after Carrano)
+
+		while (true) // Infinite loop exited via break Carrano code is
+						// equivalent in behavior to a "while"
+		{
+			while (cur != null) {
+				stack.push(cur);
+				cur = cur.left;
+			}
+			// What follows would be under Carrano's "else"
+			if (stack.isEmpty())
+				break; // Exit the processing loop
+			else {
+				cur = stack.pop();
+				if (front == null)
+					front = tail = cur;
+				else
+					tail = tail.right = cur;
+				cur.left = null;
+				cur = cur.right;
+			}
+		}
+		root = front;
+		return;
+	}
+
+	private void b() {
+		// FORM BALANCED TREE --- after Stout and Warren
+
+		// Pseudo-root used; real root to the right
+		Node pseudo = new Node(-1);
+		int m, nBack = NodeCount - 1;
+
+		pseudo.right = root;
+		for (m = nBack / 2; m > 0; m = nBack / 2) {
+			int j = 0;
+			Node scanner = pseudo;
+
+			for (j = 0; j < m; j++) {// leftward rotation
+				Node child = scanner.right;
+				scanner.right = child.right;
+				scanner = scanner.right;
+				child.right = scanner.left;
+				scanner.left = child;
+			} // end for j . . .
+			nBack = nBack - m - 1;
+		} // end for m . . .
+		root = pseudo.right;
+		return;
+	}
+
 }
